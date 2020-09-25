fb36ce282c57745dd866b429d3d25bcdfa656f0b
shenlonghua820909 <shlonh_163@163.com>
2020-09-25 14:34:14 +0800

    初始化提交代码首次进入版本控制

---
+++ springcloud-consumer-dept-80/src/main/java/com/kuang/springcloud/config/ConfigBean.java
@@ -0,0 +1,14 @@
+package com.kuang.springcloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ConfigBean { //@Configuration  -- spring  applicationContext.xml
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
